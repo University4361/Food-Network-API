297568170d1ce23b684e105447382edd923f65e5
Ilya Lavrov <ilyalavrov97@gmail.com>
2018-05-09 15:57:32 +0300

    initial commit

---
+++ CoreWebAPI/Models/Note.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace CoreWebAPI.Models
+{
+    public class Note
+    {
+        public Guid Id { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+
+        [Required]
+        public string Content { get; set; }
+    }
+}
---
+++ CoreWebAPI/Repositories/INoteRepository.cs
@@ -0,0 +1,15 @@
+﻿using CoreWebAPI.Models;
+using System;
+using System.Collections.Generic;
+
+namespace CoreWebAPI.Repositories
+{
+    public interface INoteRepository
+    {
+        IEnumerable<Note> GetAll();
+        Note GetNoteById(Guid id);
+        void Add(Note item);
+        void Update(Note item);
+        Note Remove(Guid id);
+    }
+}
